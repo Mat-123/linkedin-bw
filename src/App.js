60d98eb73c19dbf140cb659f6155abeab4641a93
looloops <sota.luljeta@gmail.com>
2024-04-02 17:23:43 +0200

    adds myNav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import MyNav from "./components/MyNav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MyNav />
+      <Container fluid="xl">
+        <Row>
+          <Col xs="12" md="7" lg="9">
+            {/* profilo */}
+            {/* Consigliato */}
+          </Col>
+          <Col xs="12" md="5" lg="3">
+            {/* sidebar */}
+          </Col>
+          {/* Footer */}
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
