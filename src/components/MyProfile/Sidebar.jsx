a1de615520abccc1ed6427951ecb7de3df7a593c
FR4NK798 <frankricciardi98@gmail.com>
2024-04-03 17:14:49 +0200

    commit

--- src/components/MyProfile/Sidebar.jsx
+++ src/components/MyProfile/Sidebar.jsx
@@ -50,84 +50,48 @@ const Sidebar = () => {
   return (
     <div className="sidebar mx-3">
       {/* ---------FIRST SECTION -------- */}
-      {
-        params.utente ? <div></div> :       <div className="sidebar-first-section text-start border border-1 mb-1 whiteBck">
-        <div className="pb-3  border-bottom">
-          <div className="d-flex align-content-center gap-5">
-            <h3>Lingua del profilo</h3>
-            <div className="pen-svg ms-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="#404040"
-                className="bi bi-pencil ms-5"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-              </svg>
+      {params.utente ? (
+        <div></div>
+      ) : (
+        <div className="sidebar-first-section text-start border border-1 mb-1 whiteBck">
+          <div className="pb-3  border-bottom">
+            <div className="d-flex align-content-center gap-5">
+              <h3>Lingua del profilo</h3>
+              <div className="pen-svg ms-auto">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="#404040"
+                  className="bi bi-pencil ms-5"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                </svg>
+              </div>
             </div>
+            <p className="sidebar-gray-text ">Italiano</p>
           </div>
-          <p className="sidebar-gray-text ">Italiano</p>
-        </div>
-        <div className="mt-3">
-          <div className="d-flex ">
-            <h3>Profilo pubblico e URL</h3>
-            <div className="pen-svg ms-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="#404040"
-                className="bi bi-pencil ms-5"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-              </svg>
+          <div className="mt-3">
+            <div className="d-flex ">
+              <h3>Profilo pubblico e URL</h3>
+              <div className="pen-svg ms-auto">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="#404040"
+                  className="bi bi-pencil ms-5"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                </svg>
+              </div>
             </div>
+            <p className="sidebar-gray-text ">www.linkedin.com/in/</p>
           </div>
-          <p className="sidebar-gray-text ">www.linkedin.com/in/</p>
         </div>
-      </div>
-      }
-      {/* <div className="sidebar-first-section text-start border border-1 mb-1 whiteBck">
-        <div className="pb-3  border-bottom">
-          <div className="d-flex align-content-center gap-5">
-            <h3>Lingua del profilo</h3>
-            <div className="pen-svg ms-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="#404040"
-                className="bi bi-pencil ms-5"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-              </svg>
-            </div>
-          </div>
-          <p className="sidebar-gray-text ">Italiano</p>
-        </div>
-        <div className="mt-3">
-          <div className="d-flex ">
-            <h3>Profilo pubblico e URL</h3>
-            <div className="pen-svg ms-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="#404040"
-                className="bi bi-pencil ms-5"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-              </svg>
-            </div>
-          </div>
-          <p className="sidebar-gray-text ">www.linkedin.com/in/</p>
-        </div>
-      </div> */}
+      )}
 
       {/* ----------SIDEBAR IMG----------- */}
       <div className="sidebar-img border border-1 my-1">
